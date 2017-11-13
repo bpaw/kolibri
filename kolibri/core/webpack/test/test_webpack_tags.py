ad05787cc62ea35e1a2684903526eb17e9aee1d0
bpaw <bpaw.dev@gmail.com>
2017-11-13 15:21:27 -0800

    added some POC unit tests, will refactor/clean in future

--- kolibri/core/webpack/test/test_webpack_tags.py
+++ kolibri/core/webpack/test/test_webpack_tags.py
@@ -4,13 +4,67 @@ from django.template import Template, Context
 from django.test.testcases import TestCase
 
 from .base import TestHook
+from kolibri.core.webpack.hooks import WebpackBundleHook
+import os
+import random
 
+DEFAULT_PLUGINS = [
+    # Note from Devon -
+    # Temporarily adding these here to get things working for most devs.
+    # It's not clear to me where the correct place to add them is.
+    "kolibri.plugins.management",
+    "kolibri.plugins.learn",
+    "kolibri.plugins.document_pdf_render",
+    "kolibri.plugins.html5_app_renderer",
+    "kolibri.plugins.media_player",
+    "kolibri.plugins.setup_wizard",
+    "kolibri.plugins.coach",
+    "kolibri.plugins.user",
+    "kolibri.plugins.style_guide"  # TODO - remove before merging in to a release branch
+]
 
 class KolibriTagNavigationTestCase(TestCase):
+
+    def r_flag_body(self, specified_plugins):
+        file = open(".input_file.txt", "w+")
+        file.write(specified_plugins)
+        file.close()
+
+        file = open(".input_file.txt", "r+")
+        input_file = file.read().split('\n')
+        input_file = filter(None, input_file)
+
+        result = [hook.webpack_bundle_data for hook in WebpackBundleHook().registered_hooks if (hook.webpack_bundle_data and any(hook.__module__.startswith(input) for input in input_file))]
+        return result
 
     def setUp(self):
         super(KolibriTagNavigationTestCase, self).setUp()
         self.test_hook = TestHook()
+
+    def test_r_flag_3_plugins(self):
+
+        result = self.r_flag_body("kolibri.plugins.html5_app_renderer\nkolibri.plugins.learn\nkolibri.plugins.document_pdf_render")
+
+        os.remove('./.input_file.txt')
+        self.assertEqual(len(result), 3)
+
+    def test_r_flag_random(self):
+
+        specified_plugins = ""
+        num_elems = 0
+        for plugin in DEFAULT_PLUGINS:
+            if random.random() >= .5:
+                specified_plugins += plugin + '\n'
+                num_elems += 1
+                # there's two management plugins
+                if plugin == "kolibri.plugins.management":
+                    num_elems += 1
+
+        print (specified_plugins)
+        result = self.r_flag_body(specified_plugins)
+
+        os.remove('./.input_file.txt')
+        self.assertEqual(len(result), num_elems)
 
     def test_frontend_tag(self):
         self.assertIn(
